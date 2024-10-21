e370e1b072c9e14081a2bc92125144322e0c8c39
3031249 <3031249@30312-49>
2024-10-21 11:06:05 +0900

    きど

---
+++ BookSystem/src/main/java/com/example/demo/service/Coupon3Service.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.entity.Coupon;
+import com.example.demo.repository.Coupon3Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class Coupon3Service {
+	
+	/** DI */
+	private final Coupon3Repository coupon3Repository;
+	
+	//取得したクーポンを表示
+	public	Coupon couponFindById(int id) {
+		return coupon3Repository.couponSelectById(id);
+	}
+		
+	//クーポンを使用済みにする
+	public void couponDelete(int id) {
+		coupon3Repository.couponDelete(id);
+	}
+
+}
