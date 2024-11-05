af4c381708d7492b9b401e97c3f6dc564630417f
3031249 <3031249@30312-49>
2024-11-05 12:55:01 +0900

    クーポンの有効期限追加、ダミーデータのクーポン5は期限切れ

--- BookSystem/src/main/java/com/example/demo/entity/Coupon.java
+++ BookSystem/src/main/java/com/example/demo/entity/Coupon.java
@@ -1,29 +1,30 @@
 package com.example.demo.entity;
 
+import java.time.LocalDate;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-/**
- * クーポン情報を格納するエンティティ
- */
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Coupon {
 
-	// クーポンID
-	private int id; 
+    private int id; 
+    private CouponType couponType;
+    private User user;
 
-	// クーポンの種類
-	private CouponType couponType; 
-	
-	// Userオブジェクト
-	private User user; 
-	
-	public Coupon(CouponType couponType,User user) {
-		this.couponType = couponType;
-		this.user = user;
-	}
+    // クーポンの取得日
+    private LocalDate issueDate;
+
+    // 有効期限
+    private LocalDate expirationDate;
+
+    public Coupon(CouponType couponType, User user) {
+        this.couponType = couponType;
+        this.user = user;
+        this.issueDate = LocalDate.now(); // クーポン取得日を現在日付に設定
+        this.expirationDate = issueDate.plusMonths(3); // 有効期限を3か月後に設定
+    }
 }
