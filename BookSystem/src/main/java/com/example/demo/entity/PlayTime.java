dd94d1080ce6ec8df3445b51c8364ff9d45cc196
3030931 <3030931@30309-31>
2024-11-05 12:54:54 +0900

    作業中

---
+++ BookSystem/src/main/java/com/example/demo/entity/PlayTime.java
@@ -0,0 +1,17 @@
+package com.example.demo.entity;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlayTime {
+	private int id;
+	private LocalDate lastplay;
+	private int playcount;
+
+}
